3c9a358574d9772bd51ef7748f43a58191fc7ffa
Leila <leila.alhemali@gmail.com>
2023-09-06 14:59:19 -0600

    updated logo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import dan from './dan.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dan} className="App-logo" alt="logo" />
+        <br></br>
+        <p>
+I love you        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
